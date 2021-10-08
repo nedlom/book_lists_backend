093288ce361287b854c0b1bfc06e9c3f66dd9883
Nick <nelom3@gmail.com>
2021-10-08 11:24:56 -0700

    book create route and actions

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::BooksController < ApplicationController
+
+  def index
+    books = Book.all
+    render json: books
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      render jason: book, status: :accepted
+    else
+      render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity}
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :image_url, :genre_id)
+  end
+
 end
